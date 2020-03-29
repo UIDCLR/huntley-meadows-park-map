2ebf4fc9d0bb9d68ec4a257426d63a42f957997d
Cory Leigh Rahman <crahman@wisc.edu>
2020-03-29 11:55:05 -0400

    Init park-map component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ParkMapComponent } from './park-map/park-map.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ParkMapComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParkMapComponent } from './park-map/park-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParkMapComponent
   ],
   imports: [
     BrowserModule,
